b8c4faeea9ee56dbb77aff9805fd21e1526b3a90
Summer ☀️ <git@summerm.dev>
2017-06-06 21:41:48 -0500

    Backup overwritten files

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ for f in $(find "$dir" \( -path "$dir/.git" -o -path "$dir/setup.sh" \) -prune -
 	relative_path=$(realpath --relative-to="$dir" "$f")
 	container=$(dirname "$relative_path")
 	mkdir -p "$HOME/$container"
-	cp -i "$f" "$HOME/$relative_path"
+	cp -ibS '.bak' "$f" "$HOME/$relative_path"
 
 	if [ "$container" = ".gnupg" ]; then
 		chmod 700 "$HOME/$container"
