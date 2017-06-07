60f2cb1b37da8c23e6f7dbd000d1ecee588109cc
Summer ☀️ <git@summerm.dev>
2017-06-06 19:14:12 -0500

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,10 @@
+set tabstop=4
+set shiftwidth=4
+set smartindent
+set number
+set splitbelow
+set splitright
+command W w
+command Q q
+command WQ wq
+command Wq wq
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/a/246128/2384183
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Loop through every file in source directory and sub directories except .git
+# subdirectory and this script
+IFS=$'\n' 
+for f in $(find "$dir" \( -path "$dir/.git" -o -path "$dir/setup.sh" \) -prune -o -type f -print); do
+	relative_path=$(realpath --relative-to="$dir" "$f")
+	container=$(dirname "$relative_path")
+	mkdir -p "$HOME/$container"
+	cp -i "$f" "$HOME/$relative_path"
+
+	if [ "$container" = ".gnupg" ]; then
+		chmod 700 "$HOME/$container"
+		chmod 600 "$HOME/$relative_path"
+	fi
+
+	if [ "$container" = ".local/bin" ]; then
+		chmod +x "$HOME/$relative_path"
+	fi
+done
