35a6ce1ed17dd22a879d7ad51b2b30eabe61fb45
Summer ☀️ <git@summerm.dev>
2017-06-09 22:14:34 -0500

    Modify setup script for macOS

--- setup.sh
+++ setup.sh
@@ -9,15 +9,30 @@ IFS=$'\n'
 for f in $(find "$dir" \( -path "$dir/.git" -o -path "$dir/setup.sh" \) -prune -o -type f -print); do
 	relative_path=$(realpath --relative-to="$dir" "$f")
 	container=$(dirname "$relative_path")
-	mkdir -p "$HOME/$container"
-	cp -ibS '.bak' "$f" "$HOME/$relative_path"
+	dest="$HOME/$relative_path"
+	dest_container="$HOME/$container"
+
+	mkdir -p "$dest_container"
+
+	if [ -s "$dest" ]; then
+		echo -n "Do you want to replace ~/$relative_path (backed up as *.bak)? (y/n) "
+		read -n 1 resp
+		resp=$(echo -n "$resp" | tr '[:upper:]' '[:lower:]')
+		echo "$resp"
+		if [ "$resp" = 'y' ]; then
+			mv "$dest" "$dest.bak"
+		else
+			continue
+		fi
+	fi
+	cp "$f" "$dest"
 
 	if [ "$container" = ".gnupg" ]; then
-		chmod 700 "$HOME/$container"
-		chmod 600 "$HOME/$relative_path"
+		chmod 700 "$dest_container"
+		chmod 600 "$dest"
 	fi
 
 	if [ "$container" = ".local/bin" ]; then
-		chmod +x "$HOME/$relative_path"
+		chmod +x "$dest"
 	fi
 done
