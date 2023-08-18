1e23498af2ad86d67d6a987f3a08063e6a233f22
Summer ☀️ <git@summerm.dev>
2023-08-17 18:23:09 -0600

    Check for potential args (just in case)

--- .local/share/ruby/irb_customizations.d/money_inspect_with_format.rb
+++ .local/share/ruby/irb_customizations.d/money_inspect_with_format.rb
@@ -1,7 +1,11 @@
 if Object.const_defined?(:Money)
   module MoneyInspectWithFormat
-    def inspect
-      format
+    def inspect(*args, **kwargs, &blk)
+      if args.empty? && kwargs.empty? && !blk
+        format
+      else
+        super
+      end
     end
   end
 
