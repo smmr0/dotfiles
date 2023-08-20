3a4d3af2b4b59c24f1a2a2e3263546d0bce1f5f8
Summer ☀️ <git@summerm.dev>
2023-08-19 21:02:14 -0600

    Use distinct IRB history for each Rails project

--- .local/share/ruby/irb_customizations.d/history.rb
+++ .local/share/ruby/irb_customizations.d/history.rb
@@ -1,3 +1,7 @@
 IRB.conf[:HISTORY_FILE] = Pathname.new(ENV['HOME']).join('.irb_history')
 IRB.conf[:SAVE_HISTORY] = 1_000
 IRB.conf[:EVAL_HISTORY] = 1_000 # Available via special `__` var; not saved to any file
+
+if defined?(Rails)
+  IRB.conf[:HISTORY_FILE] = Rails.root.join('log', '.irb_history')
+end
